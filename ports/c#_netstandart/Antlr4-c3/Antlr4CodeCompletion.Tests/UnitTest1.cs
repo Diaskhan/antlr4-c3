c1b7a2f12b608813348eac55611703073008737e
Diaskhan <Diaskhan@yandex.kz>
2025-10-19 00:38:58 +0500

    generate C# bindigs

---
+++ ports/c#_netstandart/Antlr4-c3/Antlr4CodeCompletion.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Antlr4CodeCompletion.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
