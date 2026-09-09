d80296f147853c2ff6f369d9d14d9b3cec52a982
Diaskhan <Diaskhan@yandex.kz>
2026-09-10 00:54:51 +0500

    delete tails sqlite

--- ports/dotnet/app/Antlr4C3.Demo/MainForm.cs
+++ ports/dotnet/app/Antlr4C3.Demo/MainForm.cs
@@ -20,7 +20,6 @@ namespace Antlr4C3.Demo
         {
             new Cpp14GrammarProvider(),
             new TSqlGrammarProvider(),
-            new SqliteGrammarProvider(),
         };
 
         private IGrammarProvider _grammar;
